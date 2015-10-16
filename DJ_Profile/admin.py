18077fd6704ff618c30b53fcde4e95f06b251e18
Chad Duffin <chadduffin01@gmail.com>
2015-10-15 19:08:33 -0700

    compressed all Django files, added Node.js/Socket.io files with working basic working server/client.

--- DJ_Profile/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import DJ_Profile
-
-admin.site.register(DJ_Profile)
--- DJ_Profile/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-	return HttpResponse("<h1>Hello, world. You're at the DJ Account page.</h1>")
